172e3e20086809c7b94099671dd3e39b463c4a36
AvoMR <avetis.mishegyan.882@my.csun.edu>
2023-02-02 15:02:34 -0800

    first assignment

---
+++ week_2/scripts/weightdata_hw_2023_02_02.R
@@ -0,0 +1,17 @@
+### This is my first script.  I am learning how to import data
+### Created by: Avetis Mishegyan
+### Created on: 2023-02-02
+###############################################
+
+### Load libraries ##########
+library(tidyverse)
+library(here)
+
+### Read in data ###
+weight_data <- read_csv(here("week_2", "data", "weightdata.csv"))
+
+### Data Analysis #####
+head(weight_data) # Looks at the top 6 lines of the data frame
+tail(weight_data) # Looks at the bottom 6 lines of the data frame
+View(weight_data) # opens a new window to look at the entire data frame
+glimpse(weight_data) # shows data in an alternative way to view()
